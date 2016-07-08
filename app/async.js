23d5fe1c4b21c6c20161e2c005482cc4dfb8b602
Peter W Borodich <pwborodich@gmail.com>
2016-07-08 02:20:06 -0400

    async functions

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    for (var i = 0; i < 10; ++i) {
+      setTimeout(function () {
+        console.log('hello world')
+      }, 100);
+    }
+    return value;
   },
-
   manipulateRemoteData: function(url) {
-
+    $.ajax(url).then(function (res) {
+      var people = res.people;
+      console.log(people);
+      var nameArray = people.map(function(obj){
+        var rObj;
+        rObj = obj.name;
+        console.log(rObj);
+        return rObj
+      });
+      return nameArray.sort();
+    })
   }
 };
+
+
